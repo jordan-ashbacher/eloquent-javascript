f7091c0ed40a2c875ef627dda1e8f86805f29901
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-08-10 15:42:39 -0500

    updates sum of range with step function

--- 04-data-structures/02-sum-of-range-step/app.js
+++ 04-data-structures/02-sum-of-range-step/app.js
@@ -1,15 +1,40 @@
+// FIRST SOLUTION
+
+// function range(start, end, step) {
+//   let numbers = [];
+//   let total = 0;
+//   let x = start;
+//   for(let i = 0; i <= ((end - start) / step); i++) {
+//     let count = x;
+//     numbers.push(count);
+//     total += count
+//     x += step;
+//   }
+//   console.log(numbers);
+//   console.log(total);
+// }
+//
+// range(100, 200, 5)
+
+
 function range(start, end, step) {
-  let numbers = [];
-  let total = 0;
-  let x = start;
-  for(let i = 0; i <= ((end - start) / step); i++) {
-    let count = x;
-    numbers.push(count);
-    total += count
-    x += step;
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i+=step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i+=step) array.push(i);
   }
-  console.log(numbers);
-  console.log(total);
+  return array
 }
 
-range(100, 200, 5)
+function sum(array) {
+  total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total
+}
+
+console.log(range(1, 10, 5));
+console.log(sum(range(1, 10, 5)));
