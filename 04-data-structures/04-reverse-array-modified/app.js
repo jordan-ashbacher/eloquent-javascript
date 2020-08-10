2c053fb9bcc84ec7bb6b459034068aac0a2def3d
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-08-10 12:10:04 -0500

    creates reverse array modified folder and intial files

---
+++ 04-data-structures/04-reverse-array-modified/app.js
@@ -0,0 +1,11 @@
+// NOT FUNCTIONAL
+
+// function reverseArrayInPlace(x) {
+//   for (let i of x) {
+//     x.unshift(i);
+//   }
+//   console.log(x);
+// }
+//
+// z = [1, 2, 3, 4, 5];
+// reverseArrayInPlace(z);
